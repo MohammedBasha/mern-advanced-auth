7476fbab3038b319fd1c3daba915d6d6975d6bff
MohammedBasha <me@mohammedbasha.com>
2026-05-18 22:35:08 +0300

    Starting the frontend setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 // import libraries
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 // Import database connection
 import { connectDB } from "./db/connectDB.js";
@@ -12,6 +13,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    }),
+);
 
 app.use(express.json());
 
